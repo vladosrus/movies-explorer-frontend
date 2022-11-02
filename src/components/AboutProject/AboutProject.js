8c4cc7303e7168cdb950eda4dce66b05e50d61a0
Vlad Chikov <chikovvlad@yandex.com>
2022-11-02 22:16:38 +0300

    feat: сверстать секцию promo главной страницы

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,3 +1,3 @@
 export default function AboutProject() {
-  return <section className="about-project"></section>;
+  return <section className="about-project" id="about-project"></section>;
 }
--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,3 +1,14 @@
+import './NavTab.css'
+
 export default function NavTab() {
-  return <section className="nav-tab"></section>;
+  return (
+  <nav className="nav-tab">
+    <ul className='nav-tab__list'>
+      <li className='nav-tab__list-item'><a href='#about-project' className='nav-tab__link'>О проекте</a></li>
+      <li className='nav-tab__list-item'><a href='#techs' className='nav-tab__link'>Технологии</a></li>
+      <li className='nav-tab__list-item'><a href='#about-me' className='nav-tab__link'>Студент</a></li>
+    </ul>
+    
+  </nav>
+  )
 }
