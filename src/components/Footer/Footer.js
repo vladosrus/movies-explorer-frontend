14325d69970ce38723bbe056dcd0cff8ed1c9ba4
Vlad Chikov <chikovvlad@yandex.com>
2022-11-04 14:07:51 +0300

    feat: сверстать footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,3 +1,36 @@
+import "./Footer.css";
+
 export default function Footer() {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <p className="footer__message">
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </p>
+      <div className="footer__container">
+        <p className="footer__copyright">&copy; 2022.</p>
+        <ul className="footer__links-list">
+          <li>
+            <a
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+              className="footer__link"
+            >
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li>
+            <a
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+              className="footer__link"
+            >
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
 }
