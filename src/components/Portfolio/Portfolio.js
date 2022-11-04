18458dc1bc373259f70e4ae4ceab3497d5b52221
Vlad Chikov <chikovvlad@yandex.com>
2022-11-04 13:31:17 +0300

    feat: сверстать секцию portfolio главной страницы

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,3 +1,45 @@
+import "./Portfolio.css";
+import arrow from "../../images/portfolio__arrow.svg";
+
 export default function Portfolio() {
-  return <section className="portfolio"></section>;
+  return (
+    <section className="portfolio">
+      <h2 className="portfolio__tab-title">Портфолио</h2>
+      <ul className="portfolio__links-list">
+        <li className="portfolio__links-list-item">
+          <a
+            href="/"
+            target="_blank"
+            rel="noreferrer"
+            className="portfolio__link"
+          >
+            Статичный сайт
+            <img src={arrow} alt="Стрелочка" className="portfolio__arrow" />
+          </a>
+        </li>
+        <li className="portfolio__links-list-item">
+          <a
+            href="/"
+            target="_blank"
+            rel="noreferrer"
+            className="portfolio__link"
+          >
+            Адаптивный сайт
+            <img src={arrow} alt="Стрелочка" className="portfolio__arrow" />
+          </a>
+        </li>
+        <li className="portfolio__links-list-item">
+          <a
+            href="/"
+            target="_blank"
+            rel="noreferrer"
+            className="portfolio__link"
+          >
+            Одностраничное приложение
+            <img src={arrow} alt="Стрелочка" className="portfolio__arrow" />
+          </a>
+        </li>
+      </ul>
+    </section>
+  );
 }
