8e679aad2f9a5e2b8e1458a66aca25aca2348de4
Vlad Chikov <chikovvlad@yandex.com>
2022-11-04 11:17:20 +0300

    feat: сверстать секцию techs главной страницы

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,4 +1,4 @@
-import "./AboutProject.css"
+import "./AboutProject.css";
 import TabTitle from "../TabTitle/TabTitle";
 
 export default function AboutProject() {
@@ -6,16 +6,42 @@ export default function AboutProject() {
     <section className="about-project" id="about-project">
       <TabTitle text={"О проекте"} />
       <ul className="about-project__stages-grid-container">
-        <li className="about-project__stages-grid-container-list-item"><h3 className="about-project__stage-title">Дипломный проект включал 5 этапов</h3></li>
-        <li className="about-project__stages-grid-container-list-item"><h3 className="about-project__stage-title">На выполнение диплома ушло 5 недель</h3></li>
-        <li className="about-project__stages-grid-container-list-item"><p className="about-project__stage-text">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p></li>
-        <li className="about-project__stages-grid-container-list-item"><p className="about-project__stage-text">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p></li>
+        <li className="about-project__stages-grid-container-list-item">
+          <h3 className="about-project__stage-title">
+            Дипломный проект включал 5 этапов
+          </h3>
+        </li>
+        <li className="about-project__stages-grid-container-list-item">
+          <h3 className="about-project__stage-title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+        </li>
+        <li className="about-project__stages-grid-container-list-item">
+          <p className="about-project__stage-text">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
+        </li>
+        <li className="about-project__stages-grid-container-list-item">
+          <p className="about-project__stage-text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
+        </li>
       </ul>
       <ul className="about-project__stages-map-grid-container">
-        <li className="about-project__stages-map-grid-container-list-item">1 неделя</li>
-        <li className="about-project__stages-map-grid-container-list-item">4 недели</li>
-        <li className="about-project__stages-map-grid-container-list-item">Back-end</li>
-        <li className="about-project__stages-map-grid-container-list-item">Front-end</li>
+        <li className="about-project__stages-map-grid-container-list-item">
+          1 неделя
+        </li>
+        <li className="about-project__stages-map-grid-container-list-item">
+          4 недели
+        </li>
+        <li className="about-project__stages-map-grid-container-list-item">
+          Back-end
+        </li>
+        <li className="about-project__stages-map-grid-container-list-item">
+          Front-end
+        </li>
       </ul>
     </section>
   );
--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,3 +1,23 @@
+import "./Techs.css";
+import TabTitle from "../TabTitle/TabTitle";
+
 export default function Techs() {
-  return <section className="techs" id="techs"></section>;
+  return (
+    <section className="techs" id="techs">
+      <div className="techs__container">
+        <TabTitle text={"Технологии"} />
+        <h2 className="techs__title">7 технологий</h2>
+        <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+        <ul className="techs__technologies-list">
+          <li className="techs__technologies-item">HTML</li>
+          <li className="techs__technologies-item">CSS</li>
+          <li className="techs__technologies-item">JS</li>
+          <li className="techs__technologies-item">React</li>
+          <li className="techs__technologies-item">Git</li>
+          <li className="techs__technologies-item">Express.js</li>
+          <li className="techs__technologies-item">mongoDB</li>
+        </ul>
+      </div>
+    </section>
+  );
 }
