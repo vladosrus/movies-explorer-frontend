f00426ae5bcedefbd49e08bdcb5bf1bf831a9f56
Vlad Chikov <chikovvlad@yandex.com>
2022-11-05 19:47:48 +0300

    feat: сверстать секцию moviescardlist и moviescard страницы с фильмами

---
+++ src/components/MoviesCard/MoviesCard.js
@@ -0,0 +1,48 @@
+import "./MoviesCard.css";
+import { useCallback, useState } from "react";
+
+export default function MoviesCard(props) {
+  const [isFavorites, setIsFavorites] = useState(false);
+  const onFavouritesButtonClick = useCallback(
+    () => setIsFavorites(!isFavorites),
+    [isFavorites]
+  );
+  const moviesCardFavouritesButtonClassName = `movies-card__favourites-button ${
+    isFavorites && "movies-card__favourites-button_active"
+  }`;
+
+  const convertMinutes = (min) => {
+    let hours = Math.floor(min / 60);
+    let minutes = min % 60;
+    return `${hours}ч${minutes}м`;
+  }
+
+  return (
+    <article className="movies-card">
+      <a
+        href={props.trailerLink}
+        target="_blank"
+        rel="noreferrer"
+        className="movies-card__trailer-link"
+      >
+        <img
+          className="movies-card__image"
+          src={props.imgLink}
+          alt={props.name}
+        />
+      </a>
+
+      <div className="movies-card__caption">
+        <div className="movies-card__caption-container">
+          <h2 className="movies-card__title">{props.name}</h2>
+          <p className="movies-card__duration">{convertMinutes(props.duration)}</p>
+        </div>
+        <button
+          className={moviesCardFavouritesButtonClassName}
+          type="button"
+          onClick={onFavouritesButtonClick}
+        />
+      </div>
+    </article>
+  );
+}
---
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -0,0 +1,25 @@
+import "./MoviesCardList.css";
+import MoviesCard from "../MoviesCard/MoviesCard";
+
+export default function MoviesCardList(props) {
+  return (
+    <section className="movies-cards">
+      <ul className="movies-cards__list">
+        {props.movies?.map((newMovie) => {
+          return (
+            <li className="movies-cards__list-item" key={newMovie.id}>
+              <MoviesCard
+                
+                imgLink={`https://api.nomoreparties.co${newMovie.image.formats.thumbnail.url}`}
+                trailerLink={newMovie.trailerLink}
+                name={newMovie.nameRU}
+                duration={newMovie.duration}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <button type="button" className="movies-cards__more-movies-card-button">Ещё</button>
+    </section>
+  );
+}
