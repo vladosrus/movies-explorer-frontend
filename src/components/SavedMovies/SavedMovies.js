1efdbb5cbfbd8aa8bba8543dd3d7c0798d665a1f
Vlad Chikov <chikovvlad@yandex.com>
2022-11-05 19:57:26 +0300

    feat: настроить страницу с сохранёнными фильмами

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,3 +1,13 @@
-export default function SavedMovies() {
-  return <article className="saved-movies-page"></article>;
+import MoviesCardList from "../MoviesCardList/MoviesCardList";
+import Preloader from "../Preloader/Preloader";
+import SearchForm from "../SearchForm/SearchForm";
+
+export default function SavedMovies(props) {
+  return (
+    <article className="saved-movies-page">
+      <SearchForm />
+      <MoviesCardList movies={props.movies} />
+      <Preloader />
+    </article>
+  );
 }
