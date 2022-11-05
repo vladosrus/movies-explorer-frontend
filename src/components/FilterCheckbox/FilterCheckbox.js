166983375e882af80e79e9cc5104f3f39b0c5d66
Vlad Chikov <chikovvlad@yandex.com>
2022-11-05 12:55:03 +0300

    feat: создать компонент filtercheckbox с кастомным чекбоксом

---
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -0,0 +1,20 @@
+import "./FilterCheckbox.css";
+import { useCallback, useState } from "react";
+
+export default function FilterCheckbox() {
+  const [isSelectedShortMovie, setIsSelectedIsShortMovie] = useState(false);
+  const onSelectShortMovie = useCallback(
+    () => setIsSelectedIsShortMovie(!isSelectedShortMovie),
+    [isSelectedShortMovie]
+  );
+
+  return (
+    <label className="checkbox">
+      <input type="checkbox" className="checkbox__default" />
+      <span className="checkbox__new-box" onClick={onSelectShortMovie}>
+        <span className={`checkbox__new-indicator ${isSelectedShortMovie ? "checkbox__new-indicator_selected" : ""}`}></span>
+      </span>
+      Короткометражки
+    </label>
+  );
+}
