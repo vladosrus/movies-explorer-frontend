96c2990de368777dee7cf5e43ec61cdfe946f9d6
Vlad Chikov <chikovvlad@yandex.com>
2022-11-06 19:45:26 +0300

    feat: сверстать страницу с ошибкой 404

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,3 +1,16 @@
+import { useHistory } from "react-router-dom";
+import "./NotFound.css"
+
 export default function NotFound() {
-  return <article className="not-found-page"></article>;
+  const history = useHistory();
+
+  return (
+    <article className="not-found-page">
+      <h1 className="not-found-page__error-code">404</h1>
+      <p className="not-found-page__message">Страница не найдена</p>
+      <button type="button" className="not-found-page__go-back-button" onClick={() => history.goBack()}>
+        Назад
+      </button>
+    </article>
+  );
 }
