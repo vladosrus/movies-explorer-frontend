c24538613674d0c2119a9c7df61e615fcd7cbcf6
Vlad Chikov <chikovvlad@yandex.com>
2022-11-06 10:54:01 +0300

    feat: добавить код прелоадера

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -1,3 +1,11 @@
+import './Preloader.css'
+
 export default function Preloader() {
-  return <section className="preloader"></section>;
+  return (
+    <section className="preloader">
+      <div className="preloader__container">
+        <span className="preloader__round"></span>
+      </div>
+    </section>
+  );
 }
