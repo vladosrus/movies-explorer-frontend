cdf55cd4709e9326dce9df86fe27cdfcb915de97
Vlad Chikov <chikovvlad@yandex.com>
2022-11-08 16:16:07 +0300

    feat: реализовать адаптив header footer и главной страницы

--- src/components/LargeText/LargeText.js
+++
@@ -1,9 +0,0 @@
-import "./LargeText.css";
-
-export default function LargeText(props) {
-  return props.isMainHeading ? (
-    <h1 className={`${props.className} large-text`}>{props.text}</h1>
-  ) : (
-    <h2 className={`${props.className} large-text`}>{props.text}</h2>
-  );
-}
