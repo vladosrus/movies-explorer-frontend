91a5a7aec2cbcdff416f79a52b2c79053ba84bb6
Vlad Chikov <chikovvlad@yandex.com>
2022-11-27 14:04:22 +0300

    fix: поменять логику отображения карточек фильмов на странице

--- src/hooks/useMoreMoviesButton.js
+++ src/hooks/useMoreMoviesButton.js
@@ -1,5 +1,4 @@
-import { useCallback } from "react";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import useWindowSize from "../hooks/useWindowSize";
 
 let arrayForHoldingMovies = [];
@@ -12,38 +11,44 @@ export default function useMoreMoviesButton(moviesArray) {
   const [next, setNext] = useState();
   const [moviesPerPage, setMoviesPerPage] = useState();
 
-  const loopWithSlice = useCallback((start, end) => {
-    const slice = moviesArray.slice(start, end);
-    slicedMovies = [...slice];
-    arrayForHoldingMovies = [...arrayForHoldingMovies, ...slicedMovies];
-    setMoviesToShow(arrayForHoldingMovies);
-  }, [moviesArray]);
+  const loopWithSlice = useCallback(
+    (start, end) => {
+      const slice = moviesArray.slice(start, end);
+      slicedMovies = [...slice];
+      arrayForHoldingMovies = [...arrayForHoldingMovies, ...slicedMovies];
+      setMoviesToShow(arrayForHoldingMovies);
+    },
+    [moviesArray]
+  );
 
   const moviesToShowAmong = useCallback(() => {
     if (width > 1200) {
-      setMoviesPerPage(4);
+      setMoviesPerPage(12);
       setNext(4);
     } else if (width <= 1200 && width >= 1118) {
-      setMoviesPerPage(3);
+      setMoviesPerPage(12);
       setNext(3);
     } else if (width <= 1117 && width >= 691) {
-      setMoviesPerPage(2);
+      setMoviesPerPage(8);
       setNext(2);
     } else {
       setMoviesPerPage(5);
-      setNext(5);
+      setNext(2);
     }
   }, [width]);
 
   useEffect(() => {
     moviesToShowAmong();
+  }, [moviesToShowAmong]);
+
+  useEffect(() => {
     arrayForHoldingMovies = [];
     loopWithSlice(0, moviesPerPage);
   }, [loopWithSlice, moviesPerPage, moviesToShowAmong]);
 
   const handleShowMoreMovies = () => {
-    loopWithSlice(next, next + moviesPerPage);
-    setNext(next + moviesPerPage);
+    loopWithSlice(moviesPerPage, next + moviesPerPage);
+    setMoviesPerPage(next + moviesPerPage);
   };
   return { handleShowMoreMovies, slicedMovies, moviesToShow, moviesPerPage };
 }
