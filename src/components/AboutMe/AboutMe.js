230b3f08ac1addb0ec361ea8cf498006c10d0a1c
Vlad Chikov <chikovvlad@yandex.com>
2023-11-24 16:29:02 +0300

    feat: добавить информацию о себе

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,19 +3,46 @@ import studentImg from "../../images/about-me__img.jpg";
 import TabTitle from "../TabTitle/TabTitle";
 
 export default function AboutMe() {
+  const yearsOld = new Date().getFullYear() - 2000;
+  const returnNumberInArray = (num) =>
+    num > 19 ? returnNumberInArray(num - 10) : num > 9 ? num - 10 : num;
+  const years = [
+    "лет",
+    "год",
+    "года",
+    "года",
+    "года",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+    "лет",
+  ];
+
   return (
     <section className="about-me" id="about-me">
       <TabTitle text={"Студент"} />
       <div className="about-me__container">
         <div className="about-me__text-container">
-          <h2 className="about-me__name">Виталий</h2>
-          <p className="about-me__profession">Фронтенд-разработчик, 30 лет</p>
+          <h2 className="about-me__name">Владислав</h2>
+          <p className="about-me__profession">
+            Фронтенд-разработчик, {yearsOld}{" "}
+            {years[returnNumberInArray(yearsOld)]}
+          </p>
           <p className="about-me__description">
-            Я родился и живу в Саратове, закончил факультет экономики СГУ. У
-            меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
-            бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
-            Контур». После того, как прошёл курс по веб-разработке, начал
-            заниматься фриланс-заказами и ушёл с постоянной работы.
+            Я родился и живу в Санкт-Петербурге. Я большой любитель спорта,
+            путешествий и активного отдыха. В ноябре 2022 окончил курсы от
+            Яндекс.Практикума и получил диплом Веб-разработчика. Сейчас нахожусь
+            в активном поиске работы в сфере IT.
           </p>
           <a
             href="https://github.com/vladosrus"
