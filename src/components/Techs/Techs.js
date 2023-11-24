e8f48eee2ed98bb760ca5eab058003f0fabee24c
Vlad Chikov <chikovvlad@yandex.com>
2023-11-24 14:52:32 +0300

    feat: добавить изученную технологию

--- src/components/Techs/Techs.js
+++ src/components/Techs/Techs.js
@@ -1,24 +1,31 @@
 import "./Techs.css";
 import TabTitle from "../TabTitle/TabTitle";
 
+const technologies = [
+  "HTML",
+  "CSS",
+  "JS",
+  "React",
+  "Git",
+  "Node.js",
+  "Express.js",
+  "mongoDB",
+];
+
 export default function Techs() {
   return (
     <section className="techs" id="techs">
       <div className="techs__container">
         <TabTitle text={"Технологии"} />
-        <h2 className="techs__title">7 технологий</h2>
+        <h2 className="techs__title">8 технологий</h2>
         <p className="techs__text">
           На курсе веб-разработки мы освоили технологии, которые применили в
           дипломном проекте.
         </p>
         <ul className="techs__technologies-list">
-          <li className="techs__technologies-item">HTML</li>
-          <li className="techs__technologies-item">CSS</li>
-          <li className="techs__technologies-item">JS</li>
-          <li className="techs__technologies-item">React</li>
-          <li className="techs__technologies-item">Git</li>
-          <li className="techs__technologies-item">Express.js</li>
-          <li className="techs__technologies-item">mongoDB</li>
+          {technologies.map((technologie) => (
+            <li className="techs__technologies-item">{technologie}</li>
+          ))}
         </ul>
       </div>
     </section>
